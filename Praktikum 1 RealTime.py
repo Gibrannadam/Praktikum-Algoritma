207c9fe24bfe9c2772f9286b6725517fd2f61a84
Gibrannadam <144821143+Gibrannadam@users.noreply.github.com>
2024-09-20 11:17:39 +0700

    Add files via upload

---
+++ Praktikum 1 RealTime.py
@@ -0,0 +1,14 @@
+nama1 = input("nama kamu siapa")
+asal1 = input("dari mana asal kamu")
+sekolah1 = input("sebelumnya kamu bersekolah dimana")
+
+
+
+# print("nama kamu siapa?", nama1)
+# print("dari mana asal kamu", nama1,"?",asal1)
+# print("sebelumnya bersekolah dimana?", sekolah)
+
+
+print("Hallp selamat siang", nama1)
+print("Wah dari", asal1)
+print("Pasti menyenangkan di", sekolah1)
